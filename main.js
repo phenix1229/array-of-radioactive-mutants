101e3dc65386d98e0a772f064393a611c548fcea
phenix1229 <paul.garay@codeimmersives.com>
2019-10-17 21:37:12 -0400

    completed most tasks

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
-function changeLast() {
-
+function changeLast(arr, val) {
+  arr[arr.length -1] = val;
 }
 
-function addMeToEnd() {
-
+function addMeToEnd(arr) {
+  arr.push('Colin');
 }
 
-function addMeToStart() {
-
+function addMeToStart(arr) {
+  arr.unshift('Colin');
 }
 
-function changeAllValuesTo() {
+function changeAllValuesTo(arr, val) {
+  let i = 0;
 
+  while(i < arr.length){
+    arr[i] = val;
+    i = i + 1;
+  }
 }
 
-function oddOrEven() {
+function oddOrEven(arr) {
+  let i = 0;
 
+  while(i < arr.length){
+    if (arr[i] % 2 === 0){
+      arr[1] = 'even';
+    } else {
+      arr[i] = 'odd';
+    }
+  }
 }
 
 function changeNextThreeToValue() {
